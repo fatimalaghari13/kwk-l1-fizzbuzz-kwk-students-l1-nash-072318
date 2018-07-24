b0e1b4580981b3000d737af729fd3f481e74397e
fatimalaghari13 <fatimalaghari13@gmail.com>
2018-07-24 23:51:08 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,10 @@
+number = 15
+if number%3 && number%5 == 0
+  puts "Fizzbuzz"
+elsif number%3 == 0
+  puts "Fizz"
+elsif number%5 == 0
+  puts "Buzz"
+else !(number%3 && number%5)
+  puts "nil"
+end
